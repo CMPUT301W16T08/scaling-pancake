1bff5c187bba2f19ee4588d10e7be47ba7807f0d
dan-rp <risticpe@ualberta.ca>
2016-02-12 13:14:55 -0700

    Create ViewProfileActivity.java

---
+++ app/src/main/java/cmput301w16t08/scaling_pancake/ViewProfileActivity.java
@@ -0,0 +1,18 @@
+package cmput301w16t08.scaling_pancake;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class ViewProfileActivity extends AppCompatActivity
+{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_view_profile);
+    }
+
+}
