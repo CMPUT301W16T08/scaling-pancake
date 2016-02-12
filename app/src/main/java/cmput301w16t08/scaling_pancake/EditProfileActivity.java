512ee9b8377e3157c2f0798474090bb2b0e1717c
dan-rp <risticpe@ualberta.ca>
2016-02-12 13:49:52 -0700

    Create EditProfileActivity.java

---
+++ app/src/main/java/cmput301w16t08/scaling_pancake/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package cmput301w16t08.scaling_pancake;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class EditProfileActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+
+}
