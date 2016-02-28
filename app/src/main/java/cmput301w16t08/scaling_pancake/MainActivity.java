1d2c19c0472f2125ecdab660d8f99109f5b63f56
Aaron <hliu11@ualberta.ca>
2016-02-27 22:07:45 -0700

    i added code in MainActivity, a MenuActivity&xml

--- app/src/main/java/cmput301w16t08/scaling_pancake/MainActivity.java
+++ app/src/main/java/cmput301w16t08/scaling_pancake/MainActivity.java
@@ -5,19 +5,60 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // set up our global controller
+    final Controller controller = (Controller) getApplicationContext();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // link views in xml to these variables
+        final EditText userinput = (EditText) findViewById(R.id.startscreen_username_et);
+        final Button loginButton = (Button) findViewById(R.id.startscreen_login_button);
+        final Button createButton = (Button) findViewById(R.id.startscreen_createprofile_button);
+
+        // if user click on the createprofile button
+        createButton.setOnClickListener(
+            new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // go to CreateProfileActivity
+                    createProfile(v);
+                }
+        });
+
+        // if user click on the login button
+        loginButton.setOnClickListener(
+            new Button.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    // store user name
+                    String userName = userinput.getText().toString();
+
+                    if (controller.login(userName)){
+                        // if username is right
+                        menu(v);
+                    }else {
+                        // if username does not exist
+                        //TODO: give user a message
+                    }
+                }
+        });
     }
 
-    public void createProfile(View view)
-    {
+    public void createProfile(View view) {
         Intent intent = new Intent(this, CreateProfileActivity.class);
+
+        startActivity(intent);
+    }
+
+    public void menu(View view){
+        Intent intent = new Intent(this, MenuActivity.class);
 
         startActivity(intent);
     }
---
+++ app/src/main/java/cmput301w16t08/scaling_pancake/MenuActivity.java
@@ -0,0 +1,28 @@
+package cmput301w16t08.scaling_pancake;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+
+    // set up our global controller
+    final Controller controller = (Controller) getApplicationContext();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        // link views in xml to these variables
+        final Button viewProfileButton = (Button) findViewById(R.id.menu_view_profile_button);
+        final Button addInsButton = (Button) findViewById(R.id.menu_add_instruments_button);
+        final Button viewInsButton = (Button) findViewById(R.id.menu_view_instruments_button);
+        final Button searchInsButton = (Button) findViewById(R.id.menu_search_instruments_button);
+    }
+
+}
